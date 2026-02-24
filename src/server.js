662437def9e817dfbc4b2d71ddd60c0cc854cce5
aurelep2005 <aurelep2005gmail.com>
2026-02-24 09:40:51 +0100

    initiatiion projet

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const app = require('./app');
+
+const PORT = 3000;
+
+mongoose.connect('mongodb://localhost:27017/pokedex')
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch(err => console.error(err));
